96e28a742f98b99e4acd66a8b06822c36afce431
keshav-rao <70850711+keshav-rao@users.noreply.github.com>
2020-11-09 21:45:16 +0530

    Create positive number in the list.py
    positive-number-in-a-list

---
+++ positive number in the list.py
@@ -0,0 +1,11 @@
+list1 =[12,-7,5,64,-14]
+
+list2 =[12,14,-95,3]
+
+new_list1=list (filter(lambda x: x>0,list1))
+
+new_list2=list (filter(lambda x: x>0,list2))
+
+print ("positive number in the list 1:", new_list1)
+
+print ("positive number in the list 2:", new_list2)
